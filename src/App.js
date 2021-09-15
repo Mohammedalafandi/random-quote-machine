2ed95bdf68511e5d8d3f2731ab9d0b2731d3b6a1
Mohammed Alafandi <alafandi@Mohammeds-MacBook-Air.local>
2021-09-15 15:40:19 +0300

    Added App.scss
    Added colorsArray.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import  React ,{useState ,useEffect}  from "react";
+import './App.scss';
+import COLORS_ARRAY from './colorsArray'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+let quoteDBUrl = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
 
 function App() {
+  const [quote, setQuote] = useState("Life isn’t about getting and having, it’s about giving and being.llo Qoute")
+  const [author, setAuthor] = useState("Kevin Kruse")
+  const [, setRandomNumber] = useState(0)
+  const [quotesArray, setQuotesArray] = useState(null)
+  const [accentColor, setAccentColor] = useState('#282c34')
+  const fetchQuotes = async (url) => {
+    const response = await fetch (url)
+    const parsedJson = await response.json()
+    setQuotesArray(parsedJson.quotes)
+  }
+
+useEffect(() => {
+  fetchQuotes(quoteDBUrl)
+}, [])
+
+const getRandomQuote = () => {
+  let randomInteger = Math.floor(quotesArray.length * Math.random()) 
+  setRandomNumber(randomInteger)
+  setAccentColor(COLORS_ARRAY[randomInteger])
+  setQuote (quotesArray[randomInteger].quote)
+  setAuthor(quotesArray[randomInteger].author)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{backgroundColor :accentColor}}>
+        <div id="quote-box" style={{color: accentColor}}>
+        <p id="text">
+          "{quote}"
+          </p>
+
+        <p id="author"> 
+          - {author} 
+         </p>
+         <div className="button">
+         <a id="tweet-quote" style={{backgroundColor :accentColor}} href={`https://twitter.com/intent/tweet?text=${quote} - ${author}`}><FontAwesomeIcon icon={faTwitter} /></a>
+         </div>
+         <button id="new-quote" style={{backgroundColor :accentColor}} onClick={()=> getRandomQuote()}> Next Quote </button>
+         
+       </div>
       </header>
     </div>
   );
